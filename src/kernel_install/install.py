8799cb7d6f64b65787518f13b197e6a89bff1895
k204230 <bergemann@dkrz.de>
2022-03-08 15:41:23 +0100

    Add eval system env variable to kernel spec

--- src/kernel_install/install.py
+++ src/kernel_install/install.py
@@ -11,6 +11,17 @@ __all__ = ["bash", "r", "python"]
 
 KERNEL_DIR = Path('~/.local/share/jupyter/kernels').expanduser()
 
+def get_env(*args):
+    """Get additional environment varialbes."""
+    env = {}
+    for arg in args:
+        value = os.environ.get(arg, "")
+        if value:
+            env[arg] = value
+    if env:
+        return dict(env=env)
+    return {}
+
 def bash(name: str = "bash", display_name: Optional[str] = None) -> Path:
     """Install bash kernel spec."""
     from bash_kernel.install import kernel_json
@@ -18,10 +29,13 @@ def bash(name: str = "bash", display_name: Optional[str] = None) -> Path:
     name = name or "bash"
     kernel_json["display_name"] = display_name or name
     kernel_json["name"] = name
+    env = get_env("EVALUATION_SYSTEM_CONFIG_FILE")
+    if env:
+        kernel_json["env"] = env["env"]
     with TemporaryDirectory() as td:
         os.chmod(td, 0o755) # Starts off as 700, not user readable
         with open(os.path.join(td, 'kernel.json'), 'w') as f:
-             json.dump(kernel_json, f, sort_keys=True)
+             json.dump(kernel_json, f, sort_keys=True, indent=3)
         KernelSpecManager().install_kernel_spec(td, name, user=True)
     return KERNEL_DIR / name
 
@@ -45,9 +59,12 @@ def r(name: str = "r", display_name: Optional[str] = None) -> Path:
 def python(name: str = "python3", display_name: Optional[str] = None) -> Path:
     """Install python3 kernel spec."""
     from ipykernel.kernelspec import install as install_kernel
+    eval_conf = os.environ.get("EVALUATION_SYSTEM_CONFIG_FILE", "")
+    env_kw = get_env("EVALUATION_SYSTEM_CONFIG_FILE")
     path = install_kernel(
         user=True,
         kernel_name=name or "python3",
-        display_name=display_name or name
+        display_name=display_name or name,
+        **env_kw
     )
     return Path(path)
